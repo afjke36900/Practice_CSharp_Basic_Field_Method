0d4b3dc3e876641c921ed277da8926fd737b78f0
afjke36900 <afjke36900@gmail.com>
2019-10-31 00:47:17 +0800

    UI更新+新增程式
    UI更新+新增程式

---
+++ UnityProject/Assets/Scripts/Car_control.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Car_control : MonoBehaviour
+{
+   [Header("移動速度")]
+   [Range(1,230)]
+   public int SpeedMove = 30;
+   [Header("旋轉速度")]
+   [Range(1, 100)]
+   public int SpeedTurn = 25;
+   [Header("汽車檔次")]
+   [Range(1, 6)]
+   public int CarShift = 2;
+   [Header("汽車品牌")]
+   public string Brand = "BMW";
+   [Header("是否煞車")]
+   public string Brakes = "Brakes";
+}
