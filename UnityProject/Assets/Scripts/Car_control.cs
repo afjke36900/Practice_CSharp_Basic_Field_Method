d5a916c883eb2ae9694050c5eee933b9ff8edf9d
afjke36900 <afjke36900@gmail.com>
2019-11-03 08:00:59 +0800

    新增腳本
    新增腳本

--- UnityProject/Assets/Scripts/Car_control.cs
+++ UnityProject/Assets/Scripts/Car_control.cs
@@ -1,18 +1,29 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Car_control : MonoBehaviour
 {
    [Header("移動速度")]
    [Range(1,230)]
-   public int SpeedMove = 30;
+   public int SpeedMove = 50;
    [Header("旋轉速度")]
    [Range(1, 100)]
-   public int SpeedTurn = 25;
+   public int SpeedTurn = 20;
    [Header("汽車檔次")]
    [Range(1, 6)]
    public int CarShift = 2;
    [Header("汽車品牌")]
    public string Brand = "BMW";
-   [Header("是否煞車")]
-   public string Brakes = "Brakes";
+   [Header("是否煞車"),Tooltip("用來判定要不要煞車")]
+   public bool Brakes;
+    [Header("汽車顏色")]
+   public int Color =color;
+
+    public Transform Car;
+
+    private void MoveCar()
+    {
+     car.Translate（Vector3.forward * Time.deltaTime,0,0）;
+     car.Translate（Vector3.up * Time.deltaTime，Space.World）;
+    }
 }
